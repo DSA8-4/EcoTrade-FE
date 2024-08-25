a06394d5a9bcf8fac0910d4d94116592e7f208da
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-08-26 00:03:20 +0900

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBwKhnKeIKEmiti6_qZyvlX6KTIWj6x3Y4",
-  authDomain: "ecotrade-530ef.firebaseapp.com",
-  projectId: "ecotrade-530ef",
-  storageBucket: "ecotrade-530ef.appspot.com",
-  messagingSenderId: "1073791213680",
-  appId: "1:1073791213680:web:2cfebc38ed5d5c7b8854b5",
-  measurementId: "G-9698DKM279",
-};
